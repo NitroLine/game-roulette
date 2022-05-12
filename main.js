78c1526b9cf1bfafce1c4e816fa62215230dcb2e
AlexMoshkov <9aq360zqmuf1@mail.ru>
2022-05-12 17:51:37 +0500

    add start chessboard, update .gitignore

--- main.js
+++ main.js
@@ -8,25 +8,35 @@ const port = 3000;
 const server = http.Server(app).listen(port);
 const io = socketIO(server);
 
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+}
+
 let queue = [];
 
 io.on("connection", (socket) => {
     let roomId = null;
     socket.on("addToQueue", (peerId) => {
-        console.log(queue);
+        console.log(queue.length);
         if (queue.length >= 1) {
-            let opponentPeer = queue.pop();
-            roomId = opponentPeer;
-            socket.join(opponentPeer);
-            socket.emit("joinGame", opponentPeer);
+            let opponentSocket = queue.pop();
+            roomId = opponentSocket.peerId;
+            socket.join(opponentSocket.peerId);
+            socket.emit("joinGame", opponentSocket.peerId);
+
+            let chessOrientation = ["white", "black"];
+            let randIndex = getRandomInt(0, 2);
+            socket.emit("startGame", chessOrientation[randIndex]);
+            opponentSocket.emit("startGame", chessOrientation[1 - randIndex]);
         } else {
             roomId = peerId;
             socket.join(peerId);
-            queue.push(peerId);
+            socket.peerId = peerId;
+            queue.push(socket);
         }
     });
     socket.on("disconnect", () => {
-        queue = queue.filter(v => v !== roomId);
+        queue = queue.filter(s => s.peerId !== roomId);
         socket.to(roomId).emit("leavePartner");
         console.log("disconnect");
     });
--- static/js/client.js
+++ static/js/client.js
@@ -1,10 +1,22 @@
 const socket = io("ws://localhost:3000");
 
+let chessboard = null;
+
 socket.on("joinGame", async (peerId) => {
     await video.connect(peerId);
 });
 
 socket.on("leavePartner", () => {
-    document.getElementById('uuid').innerHTML = "You lose. Loser";
+    // ...
     console.log("Opponent leave");
 });
+
+socket.on("startGame", (gameOrientation) => {
+    console.log(gameOrientation);
+
+    let config = {
+        position: 'start',
+        orientation: gameOrientation
+    }
+    chessboard = Chessboard('game', config);
+})
